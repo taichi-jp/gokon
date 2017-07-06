ef93859fd133a616045ba3852d0ebc6d21cb979c
taichikoumoto <sbskomoto@gmail.com>
2017-07-06 10:31:19 +0900

    Initial Commit

---
+++ gokon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gokon
+//
+//  Created by 河本泰知 on 2017/07/06.
+//  Copyright © 2017年 ut-develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
